c6dbfbab2efb752738298dfc5c8902d9cb55ecd8
agenciaazas <contato@agenciaazas.com>
2025-09-25 22:58:10 -0300

    Falta ligação com Banco de Dados

---
+++ sql.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `usuarios` (
+  `ID` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `LOGIN` varchar(50) NOT NULL,
+  `NOME` varchar(150) NOT NULL,
+  `EMAIL` varchar(150) NOT NULL,
+  `SENHA` varchar(80) NOT NULL,
+  `ATIVO` bit(1) DEFAULT b'1'
+);
+
+INSERT INTO `usuarios` (`LOGIN`, `NOME`, `EMAIL`, `SENHA`, `ATIVO`) VALUES 
+('Caio', 'Caio Eduardo Ferrari Miranda', 'ferraricaio066@gmail.com', 'carro', b'1');
